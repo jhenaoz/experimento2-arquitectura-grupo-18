fb5dc6c78870d4098fd87c75c8c095ffd55d9e9f
Alexander Mora Ruiz <alex@creci.co>
2023-03-14 00:09:40 -0500

    Agregar logica para controlar el intento de logueo

--- modelos/modelos.py
+++ modelos/modelos.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 
 class EnumTipoUsuario(enum.Enum):
-    ADMIN: str = 'ADMIN'
+    DIRECTOR_COMPRAS: str = 'DIRECTOR_COMPRAS'
     VENDEDOR: str = 'VENDEDOR'
 
 class Estado(enum.Enum):
@@ -17,7 +17,6 @@ class OrdenCompra(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     direccion = db.Column(db.String(255), nullable=True)
     fecha = db.Column(db.DateTime(timezone=True), server_default=func.now())
-    # vendedor = db.Column(db.String(255), nullable=True)
     detalle_orden = db.Column(db.String(255), nullable=True)
     estado = db.Column(db.Enum(Estado))
     vendedor_id = db.Column(db.Integer, db.ForeignKey('usuario.id'))
@@ -32,4 +31,5 @@ class Usuario(db.Model):
     blocked_account = db.Column(db.Boolean())
     blocked_time = db.Column(db.DateTime(), server_default=None)
     rol = db.Column(db.Enum(EnumTipoUsuario))
+    login_attempts = db.Column(db.Integer)
     ordenes = db.relationship('OrdenCompra', back_populates='vendedor')
--- vistas/vistas.py
+++ vistas/vistas.py
@@ -1,8 +1,9 @@
+import hashlib
 from flask import request
 from modelos import db, OrdenCompra, Usuario, EnumTipoUsuario
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required, create_access_token
-import hashlib
+from datetime import datetime, timedelta
 
 
 class VistaOrdenCompra(Resource):
@@ -27,7 +28,13 @@ class VistaSignIn(Resource):
             password_encrypted = hashlib.md5(
                 request.json["password"].encode('utf-8')).hexdigest()
             new_usuario = Usuario(
-                username=request.json["username"], password=password_encrypted, email=request.json["email"], rol=EnumTipoUsuario.VENDEDOR.value)
+                username=request.json["username"],
+                password=password_encrypted,
+                email=request.json["email"],
+                rol=EnumTipoUsuario.DIRECTOR_COMPRAS.value,
+                login_attempts=0,
+                blocked_account=0
+            )
             db.session.add(new_usuario)
             db.session.commit()
             return {"mensaje": "usuario creado exitosamente", "id": new_usuario.id}
@@ -39,18 +46,49 @@ class ViewLogIn(Resource):
     def post(self):
         password_encrypted = hashlib.md5(
             request.json["password"].encode('utf-8')).hexdigest()
-        user = Usuario.query.filter(Usuario.username == request.json["username"],
-                                    Usuario.password == password_encrypted).first()
+
+        user = Usuario.query.filter(
+            Usuario.username == request.json["username"]
+        ).first()
+
         db.session.commit()
 
-        rol = None
-        if user.rol is None:
-            rol = ''
-        else:
-            rol = user.rol.value
+        account_blocked = user.blocked_account
 
-        if user is None:
-            return "El usuario no existe", 404
+        # verificar si la no cuenta esta bloqueda o si ya pasó más de un día del bloqueo actual
+        now = datetime.now()
+        difference = datetime.utcnow() - user.blocked_time
+        available = account_blocked != True or difference.days >= 1
+
+        if user is not None:
+            if available == True:
+                if user.password == password_encrypted:
+                    # Reiniciar las variables de control de acceso
+                    if account_blocked == True:
+                        user.login_attempts = 0
+                        user.blocked_time = None
+                        user.blocked_account = False
+                        db.session.commit()
+
+                    token_de_acceso = create_access_token(identity=user.id)
+                    return {"mensaje": "Inicio de sesión exitoso", "token": token_de_acceso, "id": user.id, "rol": user.rol.value}
+
+                else:
+                    # Si la cuenta no esta bloqueada aun, debe contar los intentos
+                    if account_blocked == False:
+                        attempts = user.login_attempts or 0
+                        attempts = attempts + 1
+                        user.login_attempts = attempts
+
+                        if attempts == 3:
+                            user.blocked_account = True
+                            user.blocked_time = datetime.today()
+
+                        db.session.commit()
+                    return "Login fallido", 404
+
+            else:
+                return "Cuenta bloqueada", 404
+
         else:
-            token_de_acceso = create_access_token(identity=user.id)
-            return {"mensaje": "Inicio de sesión exitoso", "token": token_de_acceso, "id": user.id, "rol": rol}
+            return "El usuario no existe", 404
