ef233b3f69c211fd050b138e568ca95f3cfc4642
Alexander Mora Ruiz <alex@creci.co>
2023-03-13 22:27:15 -0500

    Configurar app.py y vistas login y signin

--- app.py
+++ app.py
@@ -1,31 +1,45 @@
-from .modelos import db , OrdenCompra
-from flask_restful import Resource, Api
-from flask import Flask,request
-from .vistas import VistaOrdenCompra
 import requests
 import os
-from app import create_app
 
-app = create_app('default')
+from flask import Flask, request
+from flask_jwt_extended import JWTManager
+from flask_restful import Resource, Api
+from modelos import db
+from vistas import VistaOrdenCompra, ViewLogIn, VistaSignIn
+# from app import create_app
+
+# app = create_app('default')
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///experimiento_dos.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'frase-secreta'
+app.config['PROPAGATE_EXCEPTIONS'] = True
+
 app_context = app.app_context()
 app_context.push()
 
 api = Api(app)
+
 db.init_app(app)
 db.create_all()
-    
+
 api.add_resource(VistaOrdenCompra, '/orden')
+api.add_resource(VistaSignIn, '/signin')
+api.add_resource(ViewLogIn, '/login')
 
 print('Starting server')
+
+
 @app.route('/ping')
 def ping():
     return "ok"
 
 
-
 @app.route('/kill-program')
 def fail():
     os._exit(12)
+
+jwt = JWTManager(app)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
--- vistas/vistas.py
+++ vistas/vistas.py
@@ -1,10 +1,14 @@
 from flask import request
-from ..modelos import db, OrdenCompra
+from modelos import db, OrdenCompra, Usuario, EnumTipoUsuario
 from flask_restful import Resource
+from flask_jwt_extended import jwt_required, create_access_token
+import hashlib
+
 
 class VistaOrdenCompra(Resource):
     def post(self):
-        nueva_orden = OrdenCompra(direccion=request.json['direccion'],           
+        nueva_orden = OrdenCompra(
+            direccion=request.json['direccion'],
             vendedor=request.json['vendedor'],
             detalle_orden=request.json['detalle_orden'],
             estado=request.json['estado']
@@ -12,3 +16,41 @@ class VistaOrdenCompra(Resource):
         db.session.add(nueva_orden)
         db.session.commit()
         return "Orden generada exitosamente", 201
+
+
+class VistaSignIn(Resource):
+
+    def post(self):
+        found = Usuario.query.filter(
+            Usuario.username == request.json["username"]).first()
+        if found is None:
+            password_encrypted = hashlib.md5(
+                request.json["password"].encode('utf-8')).hexdigest()
+            new_usuario = Usuario(
+                username=request.json["username"], password=password_encrypted, email=request.json["email"], rol=EnumTipoUsuario.VENDEDOR.value)
+            db.session.add(new_usuario)
+            db.session.commit()
+            return {"mensaje": "usuario creado exitosamente", "id": new_usuario.id}
+        else:
+            return "El usuario ya existe", 404
+
+
+class ViewLogIn(Resource):
+    def post(self):
+        password_encrypted = hashlib.md5(
+            request.json["password"].encode('utf-8')).hexdigest()
+        user = Usuario.query.filter(Usuario.username == request.json["username"],
+                                    Usuario.password == password_encrypted).first()
+        db.session.commit()
+
+        rol = None
+        if user.rol is None:
+            rol = ''
+        else:
+            rol = user.rol.value
+
+        if user is None:
+            return "El usuario no existe", 404
+        else:
+            token_de_acceso = create_access_token(identity=user.id)
+            return {"mensaje": "Inicio de sesión exitoso", "token": token_de_acceso, "id": user.id, "rol": rol}
